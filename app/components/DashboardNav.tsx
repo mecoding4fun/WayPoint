0108b1ba6f7b4df40719bbfcf80d7ebeae9b7578
Ramachandran <Tarunsankar4@gmail.com>
2026-09-05 11:50:21 +0530

    Deployment push

--- app/components/DashboardNav.tsx
+++ app/components/DashboardNav.tsx
@@ -20,12 +20,6 @@ export default function DashboardNavbar() {
        
 
         <div className="flex items-center gap-4">
-          <button className="w-9 h-9 rounded-lg border border-[#E4E0D6] flex items-center justify-center text-[#4A5C7A] hover:bg-[#FAF9F4] transition-colors">
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <path d="M18 8a6 6 0 10-12 0c0 7-3 9-3 9h18s-3-2-3-9" />
-              <path d="M13.73 21a2 2 0 01-3.46 0" />
-            </svg>
-          </button>
         <UserMenu/>
         </div>
 
